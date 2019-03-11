60f679143f9090ff416d8f5748f866ea31a4895a
Alberto <info@androiddesdecero.com>
2019-03-11 10:10:30 +0100

    05

---
+++ app/src/main/java/com/androiddesdecero/daggeranexo/ui/ThirdActivity.java
@@ -0,0 +1,15 @@
+package com.androiddesdecero.daggeranexo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androiddesdecero.daggeranexo.R;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
